3976812bd550219eafdce8e01b20e0ffb1a6a008
Mukhtar <mukhtartanimugarba@gmail.com>
2021-10-14 06:33:43 +0100

    Update file

--- M121: The MongoDB Aggregation Framework/aggregation_structure.js
+++ M121: The MongoDB Aggregation Framework/aggregation_structure.js
@@ -54,3 +54,52 @@ db.solarSystem.aggregate([{"$project": { "_id": 0, "name": 1, "surfaceGravity": 
 
 // creating a new field ``myWeight`` using expressions
 db.solarSystem.aggregate([{"$project": { "_id": 0, "name": 1, "myWeight": { "$multiply": [ { "$divide": [ "$gravity.value", 9.8 ] }, 86 ] } }}]);
+
+
+
+// **m121/addFields_stage.js**
+
+// reassign ``gravity`` field value
+db.solarSystem.aggregate([{"$project": { "gravity": "$gravity.value" } }]);
+
+// adding ``name`` and removing ``_id`` from projection
+db.solarSystem.aggregate([{"$project": { "_id": 0, "name": 1, "gravity": "$gravity.value" } }]);
+
+// adding more fields to the projected document
+db.solarSystem.aggregate([
+{"$project":{
+    "_id": 0,
+    "name": 1,
+    "gravity": "$gravity.value",
+    "meanTemperature": 1,
+    "density": 1,
+    "mass": "$mass.value",
+    "radius": "$radius.value",
+    "sma": "$sma.value" }
+}]);
+
+// using ``$addFields`` to generate the new computed field values
+db.solarSystem.aggregate([
+{"$addFields":{
+    "gravity": "$gravity.value",
+    "mass": "$mass.value",
+    "radius": "$radius.value",
+    "sma": "$sma.value"}
+}]);
+
+// combining ``$project`` with ``$addFields``
+db.solarSystem.aggregate([
+{"$project": {
+    "_id": 0,
+    "name": 1,
+    "gravity": 1,
+    "mass": 1,
+    "radius": 1,
+    "sma": 1}
+},
+{"$addFields": {
+    "gravity": "$gravity.value",
+    "mass": "$mass.value",
+    "radius": "$radius.value",
+    "sma": "$sma.value"
+}}]);
